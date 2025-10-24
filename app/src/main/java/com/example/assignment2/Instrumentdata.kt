4139088656a718c6ab5cdcabf0983aaa2acadb7b
unknown <hoangtrongquang22112006@gmail.com>
2025-10-24 09:41:43 +0700

    Initial upload for Assignment 2

---
+++ app/src/main/java/com/example/assignment2/Instrumentdata.kt
@@ -0,0 +1,8 @@
+package com.example.assignment2
+
+val instrumentList = listOf(
+    Instrument("Guitar", "String", 4.5f, 400),
+    Instrument("Piano", "Keyboard", 5.0f, 2000),
+    Instrument("Flute", "Wind", 4.0f, 50),
+    Instrument("Drumset", "Percussion", 4.3f, 550),
+)
