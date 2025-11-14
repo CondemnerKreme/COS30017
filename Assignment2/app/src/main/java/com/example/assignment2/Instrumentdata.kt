1627b4aa269768aabfc28bd94086329067d883ba
unknown <hoangtrongquang22112006@gmail.com>
2025-11-14 15:57:53 +0700

    Add Week1 and Week2 folders

--- Assignment2/app/src/main/java/com/example/assignment2/Instrumentdata.kt
+++
@@ -1,8 +0,0 @@
-package com.example.assignment2
-
-val instrumentList = listOf(
-    Instrument("Guitar", "String", 4.5f, 400),
-    Instrument("Piano", "Keyboard", 5.0f, 2000),
-    Instrument("Flute", "Wind", 4.0f, 50),
-    Instrument("Drumset", "Percussion", 4.3f, 550),
-)
